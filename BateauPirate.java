89c693cda85f413b0b1bd29877d873aa569e835f
Titouan Laville <titouanlaville1@gmail.com>
2026-05-05 17:06:08 +0200

    ajout de deplace_rand

--- BateauPirate.java
+++ BateauPirate.java
@@ -15,4 +15,23 @@ public class BateauPirate extends Bateau{
     public String getNom(){
         return (nom);
     }
+
+    public void deplace_rand(){
+        if (Math.random()<0.5){
+            if (Math.random()<0.5){
+                this.deplacer(x+1,y);
+            }
+            else{
+                this.deplacer(x-1,y);
+            }
+        }
+        else{
+            if (Math.random()<0.5){
+                this.deplacer(x,y+1);
+            }
+            else{
+                this.deplacer(x,y-1);
+            }
+        }
+    }
 }
